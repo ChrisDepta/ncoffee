8a2fab6f5aa5d0555050cf24b54e03155b387a24
Chris Depta <christophdepta@gmail.com>
2023-11-20 11:18:52 +0100

    change file name

--- src/app/dispenser/page.tsx
+++ src/app/dispenser/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Navbar from "@/components/common/nav"
 import WaterData from "@/components/sections/waterData/waterData"
 import bgWaterHand from "@/../public/bgWaterHand.webp"
-import waterwave from "@/../public/waterwave.webp"
+import waterwave from "@/../public/waterWave.webp"
 import Footer from '@/components/common/footer'
 
 
