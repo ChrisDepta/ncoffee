1b3f00a704958d723054a6c80a553c2ac12e4616
Chris Depta <christophdepta@gmail.com>
2024-01-16 14:03:52 +0100

    add portrait background for startseite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white to-nbluehome'>
-        <div className="homePageLogo w-screen flex items-center justify-center  ">
+        <div className="homePageLogoVertical md:homePageLogoHorizontal w-screen flex items-center justify-center  ">
           <main className='h-screen w-10/12 flex items-end justify-around '>
             <Link href="water" className='text-7xl tracking-wider text-white font-black m-24 hover:scale-110 transition' >woda</Link>
             <Link href="coffee" className=' text-7xl tracking-wider text-white font-black m-24 hover:scale-110 transition'>kawa</Link>
