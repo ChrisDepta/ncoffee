9df361dcbf7a4a8d8f3420b5dfa0d4bd82242454
Christopher Depta <christophdepta@gmail.com>
2024-01-17 00:07:31 +0100

    h-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white to-nbluehome'>
         <div className="homePageLogoVertical sm:homePageLogoHorizontal w-screen flex items-center justify-center  ">
-          <main className=' z-20 h-full w-10/12 flex xs:flex-col md:flex-row xs:items-center xs:justify-between md:items-end md:justify-around '>
+          <main className=' z-20 h-screen w-10/12 flex xs:flex-col md:flex-row xs:items-center xs:justify-between md:items-end md:justify-around '>
             <Link href="water" className='xs:text-4xl md:text-7xl tracking-wider text-white font-black xs:m-12 md:m-24 hover:scale-110 transition' >woda</Link>
             <Link href="coffee" className='xs:text-4xl  md:text-7xl tracking-wider text-white font-black xs:m-12 md:m-24 hover:scale-110 transition'>kawa</Link>
           </main>
