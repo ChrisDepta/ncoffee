4968fb7c04c79ea7e3837b4942e9d724a6079bee
Christopher Depta <christophdepta@gmail.com>
2024-01-16 23:57:54 +0100

    a little changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white to-nbluehome'>
-        <div className="homePageLogoVertical md:homePageLogoHorizontal w-screen flex items-center justify-center  ">
-          <main className='h-screen w-10/12 flex items-end justify-around '>
-            <Link href="water" className='text-7xl tracking-wider text-white font-black m-24 hover:scale-110 transition' >woda</Link>
-            <Link href="coffee" className=' text-7xl tracking-wider text-white font-black m-24 hover:scale-110 transition'>kawa</Link>
+        <div className="homePageLogoVertical sm:homePageLogoHorizontal w-screen flex items-center justify-center  ">
+          <main className=' z-20 h-screen w-10/12 flex xs:flex-col md:flex-row xs:items-center xs:justify-between md:items-end md:justify-around '>
+            <Link href="water" className='xs:text-4xl md:text-7xl tracking-wider text-white font-black xs:m-12 md:m-24 hover:scale-110 transition' >woda</Link>
+            <Link href="coffee" className='xs:text-4xl  md:text-7xl tracking-wider text-white font-black xs:m-12 md:m-24 hover:scale-110 transition'>kawa</Link>
           </main>
         </div>
       </div>
