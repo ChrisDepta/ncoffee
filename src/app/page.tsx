b8b39ac38ba9b4166f46a78c13d09a93a2cfe3be
Chris Depta <christophdepta@gmail.com>
2024-02-01 11:06:28 +0100

    add animation to start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,28 @@
 'use client'
 
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { easeIn, backIn, motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '@/../public/logoTurkis.webp';
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setLoaded(true);
-    }, 2200);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
+ 
   return (
     <>
      <motion.div 
-    className={`z-30 absolute top-0 left-0 w-screen h-screen flex justify-center items-center transition-opacity `}
-    style={{display: loaded ? 'none' : 'flex'}}
+    className={`z-30 absolute top-0 left-0 w-screen h-screen transition-opacity `}
     initial={{ opacity: 1 , background: 'black'}}
     animate={{ opacity: 0 }}
-    transition={{duration: 2}}
+    transition={{duration: 0.2, delay: 3, easeIn}}
     >
-      <Image src={Logo} alt="logo" width={200} height={200} className='' />
+    </motion.div>
+    <motion.div 
+      className='z-40 absolute top-0 w-screen h-screen flex justify-center items-center transition-opacity'
+      initial={{opacity: 0}}
+      animate={{opacity: [0 , 1 , 0]}}
+      transition={{duration: 3, backIn , delay: 0.2}}>
+    
+      <Image src={Logo} alt="logo" width={300} height={200} className='' />
     </motion.div>
     <div
       className={`bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white to-nbluehome `}>
