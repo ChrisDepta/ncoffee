8e9c7c4e84fdb3b92e67555703dd0a07fab3fa9e
Christopher Depta <christophdepta@gmail.com>
2024-03-25 17:35:42 +0100

    any

--- src/components/sections/coffeeData/coffeeCard.tsx
+++ src/components/sections/coffeeData/coffeeCard.tsx
@@ -16,12 +16,13 @@ type Props = {
 
 export default function coffeeCard({id, title, par1, subtitle, li1, li2, li3, price, value, picture}: Props) {
   return (
-    <div key={id} className='h-[100svh] md:h-auto bg-gradient-to-br from-nbeige/70 to-white md:mt-32 border-none md:border-0 border-nturkis rounded-none md:rounded-xl shadow-none md:shadow-2xl basis-full md:basis:auto flex flex-col justify-center items-center text-xl font-bold'>
-          <div className=' basis-full w-[80%] md:w-64 flex justify-center items-end p-0 md:p-6 '>
-            <Image src={require(`@/../public/coffees/${picture}.webp`)} width={800} height={800} className='h-auto md:h-56 w-auto hover:scale-110 transition' alt={picture} />
+    <div key={id} className='h-[100svh] md:h-auto bg-gradient-to-br from-nbeige/70 to-white md:mt-32 border-none md:border-0 border-nturkis rounded-none md:rounded-xl shadow-none md:shadow-2xl basis-full md:basis-auto flex flex-col justify-center items-center text-xl font-bold'>
+          <div className=' basis-full w-[80%] md:w-64 flex flex-col justify-end items-center p-0 md:p-6 '>
+            <p className='block md:hidden text-2xl text-norange uppercase font-extrabold mb-16'>{title}</p>
+            <Image src={require(`@/../public/coffees/${picture}.webp`)} width={800} height={800} className='h-[70svh] md:h-56 w-auto hover:scale-110 transition' alt={picture} />
           </div>
           <div className='basis-auto w-full bg-white rounded-none md:rounded-b-xl flex flex-col justify-end items-center'>
-            <p className='text-2xl text-norange uppercase font-extrabold m-4'>{title}</p>
+            <p className='hidden md:block text-2xl text-norange uppercase font-extrabold m-4'>{title}</p>
             <p className='hidden md:block capitalize mb-4'>{subtitle}</p>
           </div>
         </div>
