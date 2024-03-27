e407e22618382cb4bc804cd288f1e2cfda5abc02
Christopher Depta <christophdepta@gmail.com>
2024-03-27 22:38:27 +0100

    a little change of db.json file and articlePage

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,27 +1,23 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-
-import logoturkis from '@/../public/logoTurkis.webp'
-import logoorange from '@/../public/logoOrange.webp'
-import fb from '@/../public/fbIcon.webp'
-import insta from '@/../public/instaIcon.webp'
-import coffeebeanswave from '@/../public/coffeebeanswave.webp'
-import waterwave from "@/../public/waterWave.webp"
-
-
-
+import logoturkis from "@/../public/logoTurkis.webp";
+import logoorange from "@/../public/logoOrange.webp";
+import fb from "@/../public/fbIcon.webp";
+import insta from "@/../public/instaIcon.webp";
+import coffeebeanswave from "@/../public/coffeebeanswave.webp";
+import waterwave from "@/../public/waterWave.webp";
 
 export default function Footer() {
   const pathname = usePathname();
   let defaultColor = "";
   let defaultLogo = logoturkis;
   let defaultwave = waterwave;
-  if (pathname === '/water' || pathname === '/dispenser'){
+  if (pathname === "/water" || pathname === "/dispenser") {
     defaultColor = "text-nturkis";
     defaultLogo = logoturkis;
     defaultwave = waterwave;
@@ -30,49 +26,151 @@ export default function Footer() {
     defaultLogo = logoorange;
     defaultwave = coffeebeanswave;
   }
-  
-
 
   return (
     <>
-    <Image src={defaultwave} width={800} height={800} className='overflow-x-hidden z-10 w-screen ' priority alt={'BgOffice'}/>
-    <div className=' overflow-x-hidden z-10 text-white px-36 py-24 flex flex-wrap items-start justify-around w-screen bg-black'>
-      <div className='flex flex-col items-end'>
-      <Image src={defaultLogo} width={100} height={100} className=' m-0 sm:m-6 mt-2 w-auto h-auto' priority alt={'ncoffee makes your day'}/>
+      <Image
+        src={defaultwave}
+        width={800}
+        height={800}
+        className="overflow-x-hidden z-10 w-screen "
+        priority
+        alt={"BgOffice"}
+      />
+      <div className=" overflow-x-hidden z-10 text-white px-36 py-24 flex flex-wrap items-start justify-around w-screen bg-black">
+        <div className="flex flex-col items-end">
+          <Image
+            src={defaultLogo}
+            width={100}
+            height={100}
+            className=" m-0 sm:m-6 mt-2 w-auto h-auto"
+            priority
+            alt={"ncoffee makes your day"}
+          />
+        </div>
+        <div className="text-xl mt-8 sm:mt-0 sm:ml-2">
+          <p
+            className={`text-2xl hidden sm:block  ${defaultColor} font-bold tracking-widest`}>
+            Social
+          </p>
+          <ul className="mt-8 ">
+            <li className="w-32 mt-6 flex justify-between">
+              <Image
+                src={fb}
+                width={30}
+                height={30}
+                priority
+                alt={"facebook"}
+              />
+              <Link
+                href="https://www.facebook.com/ncoffeemakesyourday"
+                target="_blank"
+                className=" ml-4 hover:text-nlila hover:scale-110 transition">
+                Facebook
+              </Link>
+            </li>
+            <li className="w-32 mt-6 flex justify-between">
+              <Image
+                src={insta}
+                width={30}
+                height={30}
+                priority
+                alt={"instagram"}
+              />
+              <Link
+                href="https://www.instagram.com/ncoffee.your.day/"
+                target="_blank"
+                className="ml-4 hover:text-nlila hover:scale-110 transition">
+                Instagram
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="text-xl hidden xl:block">
+          <p className={`text-2xl ${defaultColor} font-bold tracking-widest`}>
+            Produkty
+          </p>
+          <ul className="mt-8 ">
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/coffee"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Kawa
+              </Link>
+            </li>
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/water"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Woda
+              </Link>
+            </li>
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="https://oryginalnysok.pl/"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Soki
+              </Link>
+            </li>
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/chemistry"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Chemia
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="text-xl hidden xl:block">
+          <p className={`text-2xl ${defaultColor} font-bold tracking-widest`}>
+            Maszyny
+          </p>
+          <ul className="mt-8 ">
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/machines"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Ekspresy
+              </Link>
+            </li>
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/dispenser"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Dystrybutory
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="text-xl hidden xl:block">
+          <p className={`text-2xl ${defaultColor} font-bold tracking-widest`}>
+            firma
+          </p>
+          <ul className="mt-8 ">
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/aboutus"
+                className=" hover:text-nlila hover:scale-110 transition">
+                O nas
+              </Link>
+            </li>
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/clients"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Klienci
+              </Link>
+            </li>
+            <li className="w-32 mt-6 flex justify-between">
+              <Link
+                href="/contact"
+                className=" hover:text-nlila hover:scale-110 transition">
+                Kontakt
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className='text-xl mt-8 sm:mt-0 sm:ml-2'>
-        <p className={`text-2xl hidden sm:block  ${defaultColor} font-bold tracking-widest`}>Social</p>
-        <ul className='mt-8 '>
-          <li className='w-32 mt-6 flex justify-between'><Image src={fb} width={30} height={30} priority alt={'facebook'}/><Link href="https://www.facebook.com/ncoffeemakesyourday" target="_blank" className=' ml-4 hover:text-nlila hover:scale-110 transition' >Facebook</Link></li>
-          <li className='w-32 mt-6 flex justify-between'><Image src={insta} width={30} height={30} priority alt={'instagram'}/><Link href="https://www.instagram.com/ncoffee.your.day/" target="_blank" className='ml-4 hover:text-nlila hover:scale-110 transition' >Instagram</Link></li>
-        </ul>
-      </div>
-      <div className='text-xl hidden xl:block'>
-        <p className={`text-2xl ${defaultColor} font-bold tracking-widest`}>Produkty</p>
-        <ul className='mt-8 '>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/coffee" className=' hover:text-nlila hover:scale-110 transition'>Kawa</Link></li>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/water" className=' hover:text-nlila hover:scale-110 transition'>Woda</Link></li>
-          <li className='w-32 mt-6 flex justify-between'><Link href="https://oryginalnysok.pl/" className=' hover:text-nlila hover:scale-110 transition'>Soki</Link></li>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/chemistry" className=' hover:text-nlila hover:scale-110 transition'>Chemia</Link></li>
-
-        </ul>
-      </div>
-      <div className='text-xl hidden xl:block'>
-        <p className={`text-2xl ${defaultColor} font-bold tracking-widest`}>Maszyny</p>
-        <ul className='mt-8 '>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/machines" className=' hover:text-nlila hover:scale-110 transition'>Ekspresy</Link></li>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/dispenser" className=' hover:text-nlila hover:scale-110 transition'>Dystrybutory</Link></li>
-        </ul>
-      </div>
-      <div className='text-xl hidden xl:block'>
-        <p className={`text-2xl ${defaultColor} font-bold tracking-widest`}>firma</p>
-        <ul className='mt-8 '>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/aboutus" className=' hover:text-nlila hover:scale-110 transition'>O nas</Link></li>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/clients" className=' hover:text-nlila hover:scale-110 transition'>Klienci</Link></li>
-          <li className='w-32 mt-6 flex justify-between'><Link href="/contact" className=' hover:text-nlila hover:scale-110 transition'>Kontakt</Link></li>
-        </ul>
-      </div>
-    </div>
     </>
-  )
+  );
 }
--- src/components/sections/articlePage/articlePage.tsx
+++ src/components/sections/articlePage/articlePage.tsx
@@ -1,6 +1,7 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Description from "@/components/elements/Description";
 
 interface Props {
   id: number;
@@ -10,17 +11,69 @@ interface Props {
   li1?: string;
   li2?: string;
   li3?: string;
+  li4?: string;
+  li5?: string;
+  li6?: string;
+  li7?: string;
+  li8?: string;
+  li9?: string;
+  li10?: string;
+  li11?: string;
   price?: string;
   value?: string;
   picture1: string;
   picture2: string;
   picture3: string;
   picture4: string;
+  technical: string;
+  descriptions: string;
+  waterContainer: string;
+  coffeeContainer: string;
+  groundsContainer: string;
+  power: string;
+  height: string;
+  witdh: string;
+  depth: string;
+  weight: string;
   path: string;
   folderName?: string;
 }
 
-export default function ArticlePage({ id, folderName = '', path, title, par1, subtitle, li1, li2, li3, price, value, picture1, picture2, picture3, picture4 }: Props) {
+export default function ArticlePage({
+  id,
+  folderName,
+  path,
+  title,
+  par1,
+  subtitle,
+  li1,
+  li2,
+  li3,
+  li4,
+  li5,
+  li6,
+  li7,
+  li8,
+  li9,
+  li10,
+  li11,
+  price,
+  value,
+  picture1,
+  picture2,
+  picture3,
+  picture4,
+  technical,
+  descriptions,
+  waterContainer,
+  coffeeContainer,
+  groundsContainer,
+  power,
+  height,
+  witdh,
+  depth,
+  weight,
+}: Props) {
   // Ustaw początkowy obraz na picture1
   const [activePicture, setActivePicture] = useState(picture1);
   const [activeModal, setActiveModal] = useState(false);
@@ -28,12 +81,12 @@ export default function ArticlePage({ id, folderName = '', path, title, par1, su
   useEffect(() => {
     if (activeModal) {
       // Dodaj styl blokujący scroll do <body> gdy modal jest aktywny
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
       // Usuń styl blokujący scroll, gdy modal jest zamknięty
-      document.body.style.overflow = '';
+      document.body.style.overflow = "";
     }
-  }, [activeModal]); 
+  }, [activeModal]);
   // Funkcja do zmiany aktywnego obrazu
   const changePicture = (pictureName: string) => () => {
     setActivePicture(pictureName);
@@ -48,36 +101,102 @@ export default function ArticlePage({ id, folderName = '', path, title, par1, su
   };
 
   return (
-    <div className='z-40 bg-white flex justify-center items-center w-full min-h-[100vh]'>
-    <div className=' flex justify-center items-start '>
-      <div className='basis-1/12 max-h-[30vw] flex flex-col justify-start items-center gap-12 mr-12 overflow-y-scroll custom-scroll'>
-        <Image onClick={changePicture(picture1)} src={require(`@/../public/${folderName}/${picture1}.webp`)} alt="title" width={100} height={200} className=' bg-black/10 p-2 hover:scale-105 transition-all' />
-        {picture2 !== "" && <Image onClick={changePicture(picture2)} src={require(`@/../public/${folderName}/${picture2}.webp`)} alt="title" width={100} height={200} className=' bg-black/10 p-2 hover:scale-105 transition-all' />}
-        {picture3 !== "" && <Image onClick={changePicture(picture3)} src={require(`@/../public/${folderName}/${picture3}.webp`)} alt="title" width={100} height={200} className=' bg-black/10 p-2 hover:scale-105 transition-all' />}
-        {picture4 !== "" && <Image onClick={changePicture(picture4)} src={require(`@/../public/${folderName}/${picture4}.webp`)} alt="title" width={100} height={200} className=' bg-black/10 p-2 hover:scale-105 transition-all' />}
-      </div>
-      <div className='basis-[30vw] h-[30vw] bg-black/10 border flex items-center justify-center p-8 mr-12'>
-        <Image onClick={openModal(true)} src={require(`@/../public/${folderName}/${activePicture}.webp`)} alt="title" width={300} height={200} className='h-full w-auto ' />
-        {activeModal && (
-        <div className='fixed inset-0 bg-black bg-opacity-90 backdrop-blur flex justify-center items-center' onClick={closeModal}>
-          <div className=' h-[90svh] mt-16 flex items-start gap-32' onClick={(e) => e.stopPropagation()}>
-            <Image  src={require(`@/../public/${folderName}/${activePicture}.webp`)} alt="title" width={600} height={400} className='h-full w-auto' />
-            <button onClick={closeModal} className='absolute top-20 right-6 p-4 rounded-full bg-norange w-auto flex justify-center items-center font-extrabold text-2xl text-white hover:scale-105 transition-all hover:bg-white hover:text-norange'>x</button>
-          </div>
+    <div className="z-40 bg-white flex justify-center items-center w-full min-h-[100vh]">
+      <div className=" flex justify-center items-start ">
+        <div className="basis-1/12 max-h-[30vw] flex flex-col justify-start items-center gap-12 mr-12 overflow-y-scroll custom-scroll">
+          <Image
+            onClick={changePicture(picture1)}
+            src={require(`@/../public/${folderName}/${picture1}.webp`)}
+            alt="title"
+            width={100}
+            height={200}
+            className=" bg-black/10 p-2 hover:scale-105 transition-all"
+          />
+          {picture2 !== "" && (
+            <Image
+              onClick={changePicture(picture2)}
+              src={require(`@/../public/${folderName}/${picture2}.webp`)}
+              alt="title"
+              width={100}
+              height={200}
+              className=" bg-black/10 p-2 hover:scale-105 transition-all"
+            />
+          )}
+          {picture3 !== "" && (
+            <Image
+              onClick={changePicture(picture3)}
+              src={require(`@/../public/${folderName}/${picture3}.webp`)}
+              alt="title"
+              width={100}
+              height={200}
+              className=" bg-black/10 p-2 hover:scale-105 transition-all"
+            />
+          )}
+          {picture4 !== "" && (
+            <Image
+              onClick={changePicture(picture4)}
+              src={require(`@/../public/${folderName}/${picture4}.webp`)}
+              alt="title"
+              width={100}
+              height={200}
+              className=" bg-black/10 p-2 hover:scale-105 transition-all"
+            />
+          )}
         </div>
-      )}
+        <div className="basis-[30vw] h-[30vw] bg-black/10 border flex items-center justify-center p-8 mr-12">
+          <Image
+            onClick={openModal(true)}
+            src={require(`@/../public/${folderName}/${activePicture}.webp`)}
+            alt="title"
+            width={300}
+            height={200}
+            className="h-full w-auto "
+          />
+          {activeModal && (
+            <div
+              className="fixed inset-0 bg-black bg-opacity-90 backdrop-blur flex justify-center items-center"
+              onClick={closeModal}>
+              <div
+                className=" h-[90svh] mt-16 flex items-start gap-32"
+                onClick={(e) => e.stopPropagation()}>
+                <Image
+                  src={require(`@/../public/${folderName}/${activePicture}.webp`)}
+                  alt="title"
+                  width={600}
+                  height={400}
+                  className="h-full w-auto"
+                />
+                <button
+                  onClick={closeModal}
+                  className="absolute top-20 right-6 p-4 rounded-full bg-norange w-auto flex justify-center items-center font-extrabold text-2xl text-white hover:scale-105 transition-all hover:bg-white hover:text-norange">
+                  x
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="text-lg basis-1/3 flex flex-col items-start justify-start">
+          <p className="text-2xl font-extrabold border-b border-norange">
+            {title}
+          </p>
+          <p className="text-xl font-bold my-1">{subtitle}</p>
+          <p className="">{par1}</p>
+          <p className="my-2">{descriptions}</p>
+          <p className="">{li1}</p>
+          <p className="">{li2}</p>
+          <p className="">{li3}</p>
+          <p className="">{li4}</p>
+          <p className="">{li5}</p>
+          <p className="">{li6}</p>
+          <p className="">{li7}</p>
+          <p className="">{li8}</p>
+          <p className="">{li9}</p>
+          <p className="">{li10}</p>
+          <p className="my-1 font-bold">{li11}</p>
+          <p className="my-2">{price}</p>
+          <p className="my-2">{value}</p>
+        </div>
       </div>
-      <div className='basis-1/3 flex flex-col items-start justify-start'>
-        <p className='text-5xl font-extrabold tracking-widest'>{title}</p>
-        <p className='text-xl'>{folderName}</p>
-        <p className='text-xl'>{activePicture}</p>
-        <p className='text-xl'>{par1}</p>
-        <p className='text-xl'>{subtitle}</p>
-        <p className='text-xl'>{li1}</p>
-        <p className='text-xl'>{price}</p>
-        <p className='text-xl'>{value}</p>
-      </div>
-    </div>
     </div>
   );
 }
